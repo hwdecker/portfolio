5d94fe1708d2d2582711d8dd081e902816c839d6
Hayden Decker <hwdecker5@gmail.com>
2022-05-06 22:33:54 -0400

    Updated Settings

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)+!&oe2qcckia#_q@8s7y4o4@@-eb1qnun7468h-zk$u@-k*mp'
+#SECRET_KEY = 'django-insecure-)+!&oe2qcckia#_q@8s7y4o4@@-eb1qnun7468h-zk$u@-k*mp'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['haydenswebsite.herokuapp.com','haydendecker.com']
+ALLOWED_HOSTS = ['haydenswebsite.herokuapp.com']
 
 
 # Application definition
