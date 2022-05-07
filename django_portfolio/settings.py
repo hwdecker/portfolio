e10594ed8020331c9dfd85c7381f9b7da041763d
Hayden Decker <hwdecker5@gmail.com>
2022-05-06 22:57:07 -0400

    Updated Settings

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['haydenswebsite.herokuapp.com']
+ALLOWED_HOSTS = ['haydenswebsite.herokuapp.com','localhost']
 
 
 # Application definition
